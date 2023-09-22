0b235b124e0f17e417aa5f2eb98943fa2307f1d4
MacKenzie1976 <97097945@st.deltion.nl>
2023-09-22 11:25:56 +0200

    UI
    settings 😋😋😊😊😍😍😍

---
+++ GeenStyle/Assets/UI UX/Programming/Settings/MusicSettings.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MusicSettings : MonoBehaviour
+{
+
+
+    public AudioMixer musicMixer;
+    public float musicSaver;
+    public Slider musicSlider;
+
+    public void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("Music", musicSaver);
+    }
+    public void SetVolume(float music)
+    {
+        musicMixer.SetFloat("Music", music);
+        musicSaver = music;
+        PlayerPrefsSet();
+    }
+    public void PlayerPrefsSet()
+    {
+        PlayerPrefs.SetFloat("Music", musicSaver);
+    }
+}
