a6d7356459063857f1a03a1af41d472b512eb292
MacKenzie1976 <97097945@st.deltion.nl>
2023-11-06 15:06:22 +0100

    UI
    Adjustement

---
+++ GeenStyle/Assets/Hannah/UI UX/Programming/Other/ShopActive.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopActive : MonoBehaviour
+{
+    public GameObject shop;
+    public bool isShopActive = false;
+
+   
+
+
+
+
+
+
+
+
+}
