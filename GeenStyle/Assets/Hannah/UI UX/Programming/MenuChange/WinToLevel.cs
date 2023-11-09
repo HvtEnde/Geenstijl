dcbceebd2a1d57ea998e1aaa73340e7b22692716
MacKenzie1976 <97097945@st.deltion.nl>
2023-11-09 11:10:09 +0100

    UI
    WUN SCREEN LEVEL 2

---
+++ GeenStyle/Assets/Hannah/UI UX/Programming/MenuChange/WinToLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinToLevel : MonoBehaviour
+{
+    public void ToLevelSelect ()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void ToLevel2()
+    {
+
+    }
+}
